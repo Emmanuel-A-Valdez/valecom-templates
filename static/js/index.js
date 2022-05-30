dd562807f1831ef4c80cc2ae39dd535cd923f336
Emmanuel-A-Valdez <g17ada02@hackademy.mx>
2022-05-30 06:35:07 -0600

    Added display and footer section

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,13 @@
 // CAROUSEL
 let swiper = new Swiper('.mySwiper', {
     slidesPerView: 3,
+    autoHeight: true,
     spaceBetween: 30,
     loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: true
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
