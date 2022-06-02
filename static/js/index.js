3ae45e02d27ac917f05ede4462e29f2e0d3a710f
Emmanuel-A-Valdez <g17ada02@hackademy.mx>
2022-06-02 08:36:46 -0600

    Made product section

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,19 @@
 // CAROUSEL
 let swiper = new Swiper('.mySwiper', {
-    slidesPerView: 3,
-    autoHeight: true,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: true
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  slidesPerView: 3,
+  autoHeight: true,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
