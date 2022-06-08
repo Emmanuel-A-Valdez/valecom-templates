f35136ebab6ad13caddcca827b3555c2a6574e8b
Emmanuel-A-Valdez <g17ada02@hackademy.mx>
2022-06-08 06:02:32 -0600

    JS for description & shipping dropdowns + product img src swap

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-// CAROUSEL
+/*** CAROUSEL ***/
 let swiper = new Swiper('.mySwiper', {
   // slidesPerView: 3,
   // spaceBetween: 30,
@@ -35,3 +35,50 @@ let swiper = new Swiper('.mySwiper', {
     },
   },
 });
+
+
+/*** PRODUCTS ***/
+// PRODUCT MAIN IMG SRC CHANGE
+let mainImg = document.getElementById('product-main-img');
+let thumbnails = document.getElementsByClassName('thumbnail');
+let activeImages = document.getElementsByClassName('thumbnail-active');
+
+for (var i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener('mouseover', function () {
+
+    if (activeImages.length > 0) {
+      activeImages[0].classList.remove('thumbnail-active');
+    }
+
+    this.classList.add('thumbnail-active');
+    mainImg.src = this.src;
+    mainImg.alt = this.alt;
+  })
+}
+
+
+// DESCRIPTION SHOW ON CLICK
+document.getElementById("description-toggle").addEventListener("click", toggleDescription);
+
+function toggleDescription() {
+  let description = document.getElementById("description");
+  if (description.style.display === "none") {
+    description.style.display = "block";
+  } else {
+    description.style.display = "none";
+  }
+}
+
+// SHIPPING SHOW ON CLICK
+document.getElementById("shipping-toggle").addEventListener("click", toggleShipping);
+
+function toggleShipping() {
+  let shipping = document.getElementById("shipping");
+  if (shipping.style.display === "none") {
+    shipping.style.display = "block";
+  } else {
+    shipping.style.display = "none";
+  }
+}
+
+
